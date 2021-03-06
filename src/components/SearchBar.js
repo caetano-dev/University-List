047b0abd3c287b41fdb09ce52edadaae873a206d
Drull <pcaetanop@gmail.com>
2021-03-06 15:08:26 -0300

    Formating

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,26 +7,29 @@ function SearchBar() {
   const [data, setData] = useState("");
   const [print, setPrint] = useState(false);
 
-
   function getData(val) {
     setPrint(false);
     setData(val.target.value);
   }
+
   return (
     <>
-    <div className={styles.SearchBar}>
-      <input type="text" placeholder="Name of the country" onChange={getData} />
-      <button onClick={() => setPrint(true)}>Search</button>
+      <div className={styles.SearchBar}>
+        <input
+          type="text"
+          placeholder="Name of the country"
+          onChange={getData}
+        />
+        <button onClick={() => setPrint(true)}>Search</button>
       </div>
       <div className={styles.University}>
-      {print ? (
-        <div >
-          <h1>Showing results for: {data}</h1>
-          <University country={data} />
-        </div>
-      ) : null}
+        {print ? (
+          <div>
+            <h1>Showing results for: {data}</h1>
+            <University country={data} />
+          </div>
+        ) : null}
       </div>
-    
     </>
   );
 }
