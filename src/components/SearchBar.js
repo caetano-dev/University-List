738eb32e9fe91b76cfc3d767b30f2402dfd10d47
Drull <pcaetanop@gmail.com>
2021-03-06 14:31:25 -0300

    Adding styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { useState } from "react";
 import University from "./University";
+import styles from "../styles/components/SearchBar.module.css";
 
 function SearchBar() {
   const [data, setData] = useState("");
   const [print, setPrint] = useState(false);
 
+
   function getData(val) {
     setPrint(false);
     setData(val.target.value);
-    console.log(data);
   }
   return (
-    <div>
-      <input type="text" placeholder="Country" onChange={getData} />
+    <>
+    <div className={styles.SearchBar}>
+      <input type="text" placeholder="Name of the country" onChange={getData} />
       <button onClick={() => setPrint(true)}>Search</button>
+      </div>
+      <div className={styles.University}>
       {print ? (
-        <div>
+        <div >
           <h1>Showing results for: {data}</h1>
           <University country={data} />
         </div>
       ) : null}
-    </div>
+      </div>
+    
+    </>
   );
 }
 
