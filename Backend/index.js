ce0a904305b9d7d9c89ab5f05b90fd579ff28b89
drull1000 <pcaetanop@protonmail.com>
2021-09-16 18:33:20 -0300

    Creating getUniversities function

--- Backend/index.js
+++ Backend/index.js
@@ -3,19 +3,21 @@ const axios = require("axios");
 const app = express();
 app.use(express.json({ limit: "1mb" }));
 
+const getUniversities = async (req, res) => {
+  country = req.body.msg;
+  const url = `http://universities.hipolabs.com/search?country=${country}`;
+  let response = await axios.get(url);
+  res.send(response.data);
+};
+
 app.get("/", (req, res) => {
   res.set("Access-Control-Allow-Origin", "http://localhost:3000");
 });
 
 app.post("/api", async (req, res) => {
-  country = req.body.msg;
-  const url = `http://universities.hipolabs.com/search?country=${country}`;
-  try {
-    let response = await axios.get(url);
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  getUniversities(req, res).catch((err) => {
+    res.status(500).send(err);
+  });
 });
 
 app.listen(8080, () => {
