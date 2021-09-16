3a54e3a880f1f413ec9e912460de9a92455f2ee7
drull1000 <pcaetanop@protonmail.com>
2021-09-16 18:34:51 -0300

    Removing get

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,6 @@ const getUniversities = async (req, res) => {
   res.send(response.data);
 };
 
-app.get("/", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
-});
-
 app.post("/api", async (req, res) => {
   getUniversities(req, res).catch((err) => {
     res.status(500).send(err);
